4a8c4868869954fcd2b10f10dd35f1c327ab6fa3
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2023-08-06 16:28:39 +0200

    Finished plugin

---
+++ README.md
@@ -0,0 +1,19 @@
+# ExecSigns
+Simple Paper plugin made in Java which executes command on sign when user right clicks it.
+
+Version: 1.19.4+
+
+## How to use
+- First line should start with `<` and end with `>`
+- Second line are arguments for command
+
+### Placeholders:
+- `{player}` - player who right clicked on sign
+
+## How to compile
+- Download `Minecraft Development` plugin in `IntelliJ IDEA`
+- Build artifact, don't forget to modify path
+
+[Full video tutorial (you won't need everything)](https://www.youtube.com/watch?v=5DBJcz0ceaw)
+
+*You can also just download `exec-signs.jar` file*
--- src/main/java/me/andrej123456789/execsigns/ExecSign.java
+++ src/main/java/me/andrej123456789/execsigns/ExecSign.java
@@ -5,17 +5,17 @@ import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ExecSign extends JavaPlugin implements Listener {
-
     @Override
     public void onEnable() {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(this, this);
+
+        getLogger().info("Initialization of ExecSigns is done!");
+        getLogger().info("If you like this plugin, give it a star on Github: https://github.com/Andrej123456789/ExecSigns");
     }
 
     @Override
@@ -29,22 +29,50 @@ public final class ExecSign extends JavaPlugin implements Listener {
             Block clickedBlock = event.getClickedBlock();
             Material blockType = clickedBlock.getType();
 
+            /* Check if block is sign */
             if (blockType == Material.ACACIA_SIGN ||
                 blockType == Material.BIRCH_SIGN ||
                 blockType == Material.DARK_OAK_SIGN ||
                 blockType == Material.JUNGLE_SIGN ||
                 blockType == Material.OAK_SIGN ||
                 blockType == Material.SPRUCE_SIGN ||
+                blockType == Material.MANGROVE_SIGN ||
                 blockType == Material.WARPED_SIGN ||
-                blockType == Material.CRIMSON_SIGN) {
+                blockType == Material.CRIMSON_SIGN ||
+                blockType == Material.ACACIA_WALL_SIGN ||
+                blockType == Material.BIRCH_WALL_SIGN ||
+                blockType == Material.DARK_OAK_WALL_SIGN ||
+                blockType == Material.JUNGLE_WALL_SIGN ||
+                blockType == Material.OAK_WALL_SIGN ||
+                blockType == Material.SPRUCE_WALL_SIGN ||
+                blockType == Material.MANGROVE_WALL_SIGN ||
+                blockType == Material.WARPED_WALL_SIGN ||
+                blockType == Material.CRIMSON_WALL_SIGN) {
 
                 Sign sign = (Sign) clickedBlock.getState();
 
                 String[] lines = sign.getLines();
                 String space = " ";
 
-                getLogger().warning(lines[0] + space + lines[1]);
-                event.getPlayer().performCommand(lines[0] + space + lines[1]);
+                /* If first line starts with '<' and ends with '>', execute command */
+                if (lines[0].startsWith("<") && lines[0].endsWith(">")) {
+                    String remove = "<>";
+                    StringBuilder resultStringBuilder = new StringBuilder();
+
+                    for (char c : lines[0].toCharArray()) {
+                        if (remove.indexOf(c) == -1) {
+                            resultStringBuilder.append(c);
+                        }
+                    }
+
+                    String player_placeholder = "{player}";
+                    String player_name = event.getPlayer().getName();
+
+                    String command = resultStringBuilder.toString() + space + lines[1].replace(player_placeholder, player_name);
+                    event.getPlayer().performCommand(command);
+
+                    getLogger().info("Command '" + command + "' has been executed.");
+                }
             }
         }
     }
